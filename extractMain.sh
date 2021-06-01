c653418e6976eeaaad42b0a45c3ff42ed5705a71
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-01 13:08:28 +0200

    Added extractMain.sh

---
+++ extractMain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+PACKAGE_FILES=$1
+
+for FILE in $PACKAGE_FILES/*/package.json;
+do
+    echo $(basename $(dirname $FILE)),$(jq .main $FILE)
+done
