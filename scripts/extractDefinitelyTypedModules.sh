3995cef9cc6c65fa15254ee48eee16c0fb08054c
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-16 14:36:25 +0200

    Started implementation of getVersionFromDeclarationFile.js

--- scripts/extractDefinitelyTypedModules.sh
+++ scripts/extractDefinitelyTypedModules.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_PATH="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
 
 DEFINITELY_TYPED_REPO="https://github.com/DefinitelyTyped/DefinitelyTyped.git"
 DEFINITELY_TYPED_FOLDER=$1
 MODULES_FILE=$2
 
 echo "Extracting modules ..."
-# ls $DEFINITELY_TYPED_FOLDER/types > $MODULES_FILE
-for d in $DEFINITELY_TYPED_FOLDER/types/* ; do
-    echo $(node ../tools/getVersionFromDeclarationFileComment.js $(head -n 1 $d/index.d.ts))
+ls $DEFINITELY_TYPED_FOLDER/types > $MODULES_FILE
+for d in $DEFINITELY_TYPED_FOLDER/types/*; do
+    # echo $(node $SCRIPT_PATH/../tools/getVersionFromDeclarationFileComment.js "$(head -n 1 $d/index.d.ts)")
+    # node $SCRIPT_PATH/../tools/getVersionFromDeclarationFileComment.js "$(head -n 1 $d/index.d.ts)"
 done
 
 echo "Done"
--- tools/getVersionFromDeclarationFileComment.js
+++ tools/getVersionFromDeclarationFileComment.js
@@ -1,2 +1,8 @@
 var list = process.argv[2].split(' ')
-console.log(list[list.length - 1])
+var str = list[list.length - 1]
+if (str.includes('.') && /\d/.test(str)) {
+    // console.log(str)
+    
+} else {
+    // console.log(str)
+}
