4413a13503ba7494397e382c6d6daaa46bdf5e20
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-21 21:09:36 +0200

    Fixed extractTestScripts.sh

--- scripts/extractTestScripts.sh
+++ scripts/extractTestScripts.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_PATH="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
 
 PACKAGE_FILES=$1
 OUTPUT_FILE=$2
 
 rm -f $OUTPUT_FILE
 
-run() {
-    local FILE=$1
+for FILE in $PACKAGE_FILES/*/package.json; do (
     echo "$(basename $(dirname $FILE)),\"$(node $SCRIPT_PATH/../tools/testScriptParser.js $FILE)\""
-    echo "$(basename $(dirname $FILE)),\"$(node $SCRIPT_PATH/../tools/testScriptParser.js $FILE)\"" >> $OUTPUT_FILE
-}
-
-for FILE in $PACKAGE_FILES/*/package.json; do "$run" & done
+    echo "$(basename $(dirname $FILE)),\"$(node $SCRIPT_PATH/../tools/testScriptParser.js $FILE)\"" >>$OUTPUT_FILE
+) & done
