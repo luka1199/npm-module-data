a315aaea7ae80f50abbf06ee7d0f460793013215
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-24 12:11:02 +0200

    Added blacklist

--- scripts/cloneRepos.sh
+++ scripts/cloneRepos.sh
@@ -10,9 +10,10 @@ OUTPUT_DIRECTORY=$2
 rm -rf $OUTPUT_DIRECTORY
 mkdir $OUTPUT_DIRECTORY
 
+N=10
 while IFS=, read -r MODULE_NAME URL COMMIT_HASH
 do
-	if [[ $URL == "" ]]
+	(if [[ $URL == "" || $URL == "github.com/lodash/lodash.git" ]]
 	then	
         continue
 	fi
@@ -25,9 +26,16 @@ do
 		cd "$OUTPUT_DIRECTORY/$MODULE_NAME/src"
         git checkout $COMMIT_HASH
 		cd $CURRENT_DIR
-	fi
+	fi) &
+
+	# allow to execute up to $N jobs in parallel
+    if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+        wait -n
+    fi
 
 done < $MODULES_WITH_GITHUB_REPO
+
+wait
 
 # COUNT_EMPTY_DIRECTORIES=$(find $OUTPUT_DIRECTORY -type d -empty | wc -l | tr -d '[:space:]')
 # echo "Cleaning empty directories: $COUNT_EMPTY_DIRECTORIES ..."
